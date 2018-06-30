910f5682c3a45c8bd0cb1de6009d3a317c6b5527
Shubham Pulate <shubham.pulate@medipta.com>
2018-06-30 11:27:48 +0530

    first commit in Step Counter

---
+++ app/src/main/java/com/medipta/stepcounter/FStepService.java
@@ -0,0 +1,215 @@
+package com.medipta.stepcounter;
+
+import android.app.Service;
+import android.hardware.Sensor;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.SensorEvent;
+import android.os.Binder;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Looper;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+
+
+
+public class FStepService extends Service implements SensorEventListener {
+
+    public static final String ACTION_FLUSH_SENSOR = "net.frakbot.FStepService.FlushSensorData";
+    private SensorManager mSensorManager;
+    private Sensor mStepCounter, mStepDetector;
+    private android.os.Handler mUiHandler;
+    private ArrayList<StepCountListener> mListeners = new ArrayList<>();
+    private int mLastCount, mInitialCount;
+    private boolean mInitialCountInitialized;
+    private int mLastDetectorCount;
+
+    private final IBinder mBinder = new LocalBinder();
+    private PendingIntent mSensorUpdatePIntent;
+    private IntentFilter mIntentFilter;
+    private BroadcastReceiver mUpdateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (ACTION_FLUSH_SENSOR.equals(intent.getAction())) {
+                Log.i("FStepService", "Flushing the sensor's data");
+                mSensorManager.flush(FStepService.this);
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        Log.v("FStepService", "Creating the service");
+        super.onCreate();
+
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(ACTION_FLUSH_SENSOR);
+        registerReceiver(mUpdateReceiver, mIntentFilter);
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mStepCounter = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        mStepDetector = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+        mUiHandler = new Handler(Looper.getMainLooper());
+
+        mSensorUpdatePIntent = PendingIntent.getService(this, 1, new Intent(ACTION_FLUSH_SENSOR), 0);
+
+        mSensorManager.registerListener(this, mStepCounter, SensorManager.SENSOR_DELAY_NORMAL);
+
+        final int reportInterval = calcSensorReportInterval(mStepDetector);
+        mSensorManager.registerListener(this, mStepDetector, SensorManager.SENSOR_DELAY_NORMAL,
+                reportInterval * 1000 /*  micro seconds */);
+
+        if (reportInterval > 0) {
+            Log.i("FStepService", "Setting up batched data retrieval every " + reportInterval + " ms");
+            setupSensorUpdateAlarm(reportInterval);
+        }
+
+        else {
+            Log.w("FStepService", "This device doesn't support events batching!");
+        }
+    }
+
+    /**
+     * Calculates the maximum sensor report interval, based on the
+     * hardware sensor events buffer size, to avoid dropping steps.
+     *
+     * @param stepCounter The Step Counter sensor
+     *
+     * @return Returns the optimal update interval, in milliseconds
+     */
+    private static int calcSensorReportInterval(Sensor stepCounter) {
+
+        final int fifoSize = stepCounter.getFifoReservedEventCount();
+        if (fifoSize > 1) {
+            return (fifoSize / 2) * 1000;
+        }
+
+
+        return 0;
+    }
+
+    private void setupSensorUpdateAlarm(int interval) {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + interval,
+                interval, mSensorUpdatePIntent);
+    }
+
+    public IBinder onBind(Intent intent) {
+        Log.v("FStepService", "Binding the service");
+        return mBinder;
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.v("FStepService", "Destroying the service");
+        super.onDestroy();
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        alarmManager.cancel(mSensorUpdatePIntent);
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        final int type = event.sensor.getType();
+        if (type == Sensor.TYPE_STEP_COUNTER) {
+            Log.v("FStepService", "New step counter event. Value: " + (int)event.values[0]);
+
+            if (!mInitialCountInitialized) {
+                Log.i("FStepService", "Initializing initial steps count: " + (int) event.values[0]);
+                mInitialCount = (int) event.values[0];
+                mInitialCountInitialized = true;
+            }
+
+            mLastCount = (int) event.values[0] - mInitialCount;
+
+            postSensorChange(mLastCount, Sensor.TYPE_STEP_COUNTER);
+        }
+        else if (type == Sensor.TYPE_STEP_DETECTOR) {
+            mLastDetectorCount++;
+            Log.v("FStepService", "New step detector event. Updated count: " + mLastDetectorCount);
+
+            postSensorChange((int) mLastDetectorCount, Sensor.TYPE_STEP_DETECTOR);
+        }
+    }
+
+    /**
+     * Posts a step sensor event, both for the counter and the detector,
+     * to the registered listeners.
+     *
+     * @param value The event value, if any
+     * @param type  The sensor type
+     */
+    private void postSensorChange(final int value, final int type) {
+        if (Looper.getMainLooper().equals(Looper.myLooper())) {
+            // UI thread
+            for (StepCountListener l : mListeners) {
+                if (type == Sensor.TYPE_STEP_COUNTER) {
+                    l.onStepDataUpdate(value);
+                }
+                else if (type == Sensor.TYPE_STEP_DETECTOR) {
+                    l.onStep(value);
+                }
+            }
+        }
+        else {
+            // Non-UI thread
+            mUiHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    for (StepCountListener l : mListeners) {
+                        if (type == Sensor.TYPE_STEP_COUNTER) {
+                            l.onStepDataUpdate(value);
+                        }
+                        else if (type == Sensor.TYPE_STEP_DETECTOR) {
+                            l.onStep(value);
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    public void registerListener(StepCountListener l) {
+        mListeners.add(l);
+        Log.v("FStepService", "Feeding last known step count to newly registered listener");
+        l.onStepDataUpdate(mLastCount);
+        mSensorManager.flush(this);
+
+    }
+
+    public void unregisterListener(StepCountListener l) {
+        Log.v("FStepService", "Unregistering a listener");
+        mListeners.remove(l);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        Log.v("FStepService", "Sensor accuracy changed. New value: " + accuracy);
+    }
+
+    public class LocalBinder extends Binder {
+
+        public FStepService getService() {
+
+            return FStepService.this;
+        }
+    }
+
+    public interface StepCountListener {
+
+        void onStepDataUpdate(int stepCount);
+
+        void onStep(int count);
+
+    }
+}
---
+++ app/src/main/java/com/medipta/stepcounter/MainActivity.java
@@ -0,0 +1,130 @@
+package com.medipta.stepcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewPropertyAnimator;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+
+        startService(new Intent(this, FStepService.class));
+    }
+
+
+    public static class PlaceholderFragment extends Fragment implements FStepService.StepCountListener {
+
+        private final PlaceholderFragment.FSSConnection mFSSConnection;
+
+        private TextView mTxtStepsCount, mTxtStepsDetectedCount, mTxtStepEvent,mTxtDistance;
+        private FStepService mFSService;
+        private ViewPropertyAnimator mStepEventAnim;
+
+        public PlaceholderFragment() {
+            mFSSConnection = new FSSConnection();
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            mTxtStepsCount = (TextView) rootView.findViewById(R.id.txt_steps_count);
+            mTxtStepEvent = (TextView) rootView.findViewById(R.id.txt_step_event);
+            mTxtStepsDetectedCount = (TextView) rootView.findViewById(R.id.txt_steps_detected_count);
+            mTxtDistance=rootView.findViewById(R.id.txtDistance);
+
+
+            mTxtStepEvent.setAlpha(0f);
+
+            return rootView;
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            final Activity activity = getActivity();
+            if (activity != null) {
+                activity.bindService(new Intent(activity, FStepService.class),
+                        mFSSConnection, 0);
+            }
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+
+            if (mFSService != null) {
+                mFSService.unregisterListener(PlaceholderFragment.this);
+            }
+
+            final Activity activity = getActivity();
+            if (activity != null) {
+                activity.unbindService(mFSSConnection);
+            }
+        }
+
+        @Override
+        public void onStepDataUpdate(int stepCount) {
+            mTxtStepsCount.setText(String.valueOf(stepCount));
+            mTxtDistance.setText(String.valueOf("Distance walked : "+getDistanceRun(stepCount)+" km"));
+        }
+
+        @Override
+        public void onStep(int count) {
+            if (mStepEventAnim != null) {
+                mStepEventAnim.cancel();
+            }
+            mTxtStepsDetectedCount.setText(String.valueOf(count));
+            mTxtStepEvent.setAlpha(1f);
+            mStepEventAnim = mTxtStepEvent.animate().setDuration(500).alpha(0f);
+        }
+
+        private class FSSConnection implements ServiceConnection {
+
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                Log.d("MainActivity", "Connected to the FStepService");
+                mFSService = ((FStepService.LocalBinder) service).getService();
+                mFSService.registerListener(PlaceholderFragment.this);
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                mFSService = null;
+                Log.d("MainActivity", "The FStepService has disconnected");
+            }
+
+        }
+        public float getDistanceRun(long steps) {
+            float distance = (float) (steps * 78) / (float) 100000;
+            return distance;
+        }
+    }
+}
